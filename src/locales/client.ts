262576c067fa7cb6467223d055e054e459c49492
Chang Ken <kennethchangla@gmail.com>
2025-05-26 06:15:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./node_modules/next-international/dist/app/client
    > Invalid import
    > 'client-only' cannot be imported from a Server Component module. It should only be used from a Client Component.
    > The error was caused by importing 'node_modules/next-international/dist/app/client'
    >
    >
    > . Error source: Call Stack
    > 5
    >
    > Show 5 ignore-listed frame(s)

--- src/locales/client.ts
+++ src/locales/client.ts
@@ -1,4 +1,6 @@
 
+'use client'; // Add this directive
+
 import { createI18nClient } from 'next-international/client';
 
 export const { useI18n, useScopedI18n, I18nProviderClient, useCurrentLocale } = createI18nClient({
