2a58b2a57b31a7d7d49d303770ac743516c0da50
chang <kennethchangla@gmail.com>
2025-06-13 14:46:53 +0800

    feat/ i18n hide url in language

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import type { NextRequest } from 'next/server';
 const I18nMiddleware = createI18nMiddleware({
     locales: ['en', 'zh'],
     defaultLocale: 'zh',
+    urlMappingStrategy: 'rewrite'
 });
 
 export function middleware(request: NextRequest) {
